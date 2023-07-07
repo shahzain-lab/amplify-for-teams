2893a70fdc9d790b2b2ea0df9420e8ba2d141dd8
shahzain-lab <shahzain30196@gmail.com>
2023-07-07 06:39:06 -0700

    clone dev branch

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifytodo": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
